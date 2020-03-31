d314e6a094776467147645fe5e3aadb3319526f5
kth0522 <rlaxogh369@gmail.com>
2020-03-31 16:19:57 +0900

    double q learning

--- jsg_agent.py
+++ jsg_agent.py
@@ -1,70 +1,80 @@
 import numpy as np
 import gym
+import time
 
 class Agent:
     def __init__(self, Q, env, mode):
-        self.Q = Q
+        self.Q1 = Q
+        self.Q2 = Q
         self.env = env
         self.mode = mode
 
-        #Hyperparameter
-        self.alpha = 0.1
-        self.gamma = 0.8
+        # Hyperparameter
+        self.alpha = 0.01
+        self.gamma = 0.5
+
+        self.max_episode = 50000
+        self.max_iteration = 1000
+
         self.epsilon = 0.1
-        self.max_episode = 1000
 
         self.state_size = env.observation_space.n
         self.action_size = env.action_space.n
 
         if mode == "learning_mode":
-            #Initializing Q table
+            # Initializing Q table
             for i in range(self.state_size):
-                self.Q[i]
+                self.Q1[i]
+                self.Q2[i]
         elif mode == "testing_mode":
-            #If it is in testing mode, just use the existing Q table
+            # If it is in testing mode, just use the existing Q table
             pass
         else:
             raise NameError('Use valid mode')
 
+        # Possible actions list
         self.actions = np.arange(self.action_size)
 
-    #Epsilon-greedy exploration
+    # Epsilon-greedy exploration
     def select_action(self, state):
         if np.random.rand() > self.epsilon:
-            action = np.argmax(self.Q[state])
+            action = np.argmax(self.Q1[state]+self.Q2[state])
         else:
             action = np.random.choice(self.actions)
 
         return action
 
+    def update_Q(self, state, new_state, reward, action):
+        if np.random.rand() > 0.5:
+            self.Q1[state][action] = self.Q1[state][action] + self.alpha*(reward+self.gamma*np.max(self.Q1[new_state, action])-self.Q1[state][action])
+        else:
+            self.Q2[state][action] = self.Q2[state][action] + self.alpha * (
+                        reward + self.gamma * np.max(self.Q1[new_state, action]) - self.Q2[state][action])
     def learn(self):
         rewards = []
         for cur_episode in range(self.max_episode):
             state = self.env.reset()
             episode_reward = 0
-
-
-            #While episode is terminated
-            while True:
+            step = 0
+            # While episode is terminated
+            while True and step < self.max_iteration:
                 action = self.select_action(state)
                 new_state, reward, done, _ = self.env.step(action)
 
-                self.Q[new_state][action] = self.Q[new_state][action] + self.alpha*(reward + self.gamma*np.max(self.Q[new_state])-self.Q[state][action])
-                episode_reward += reward
-                if done:
-                    if reward == 0:
-                        reward = -1
-                    rewards.append(episode_reward)
-                    self.Q[new_state][action] = self.Q[new_state][action] + self.alpha * (
-                                reward + self.gamma * np.max(self.Q[new_state]) - self.Q[state][action])
-                    break
-                else:
-                    if reward == 0:
-                        reward = -0.1
-                    rewards.append(episode_reward)
-                    self.Q[new_state][action] = self.Q[new_state][action] + self.alpha * (
-                            reward + self.gamma * np.max(self.Q[new_state]) - self.Q[state][action])
+                if reward==0:
+                    reward=-0.01
+
+                self.update_Q(state, new_state, reward, action)
+
                 state = new_state
+                episode_reward += reward
+                step+=1
+
+                if done:
+                    break
+
+            rewards.append(episode_reward)
+
 
 
             if cur_episode%100 == 0:
